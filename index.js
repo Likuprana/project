1d95022bf8f51e6430cf96649435667dca4bfe14
Pranab Das <pranabd402@gmail.com>
2023-08-10 04:42:19 +0530

    Your commit message here

---
+++ index.js
@@ -0,0 +1,33 @@
+// const inputBox=document.getElementsByClassName("inputBox")
+// const click_button=document.getElementsByClassName("button-class")
+// const weatherimage=document.querySelector(".weather-image")
+// const tempature=document.getElementsByClassName("tempature")
+// const disperiction=document.getElementsByClassName("disperiction")
+// const humidity =document.getElementsByClassName("humidity")
+// const windSpeed =document.getElementsByClassName("windSpeed")
+
+
+// console.log(humidity);
+
+
+// async function checkweather(){
+
+//     const API_key ="36f0fdd247c212b83d7dbc8a9e082399";
+// const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`
+
+// const weather_data = await fetch(`${url}`).then(respons=>respons.json());
+
+// console.log(weather_data);
+
+// }
+
+
+
+// click_button.addEventListener("click",()=>{
+//     const a=checkweather(inputBox.value)
+//     console.log(a.value);
+// });
+
+
+
+
